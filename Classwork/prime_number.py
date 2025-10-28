8416592254332a3fdb65d5488716b2dceb9a8f76
infinite-apar <apsharma@infinite.com>
2025-10-28 13:39:19 +0545

    Prime number detection using loop and break statement

---
+++ Classwork/prime_number.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number: "))
+for i in range(2, int(number/2) + 1):
+    if number % i == 0:
+        print(f"{number} is not a prime number.")
+        is_prime = False
+        break
+
+if is_prime:
+    print(f"{number} is a prime number.")
